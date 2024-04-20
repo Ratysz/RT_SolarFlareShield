10af970c47adbdfb2fcf3c6aa37ff70fde6db9c4
Alexander Sepity <ratysz@gmail.com>
2024-04-20 13:50:40 -0400

    Bump to RimWorld 1.5.

---
+++ v1.5/Source/Patch_PowerNetTick.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RT_SolarFlareShield
+{
+	[HarmonyPatch(typeof(PowerNet))]
+	[HarmonyPatch(nameof(PowerNet.PowerNetTick))]
+	internal static class Patch_PowerNetTick
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(
+			MethodBase original,
+			IEnumerable<CodeInstruction> instructions)
+		{
+			return instructions.MethodReplacer(
+				AccessTools.Method(typeof(GameConditionManager), nameof(GameConditionManager.ElectricityDisabled)),
+				AccessTools.Method(typeof(Patch_PowerNetTick), nameof(Patch_PowerNetTick.ElectricityDisabled)));
+		}
+
+		public static bool ElectricityDisabled(this GameConditionManager instance, Verse.Map map)
+		{
+			return instance.ElectricityDisabled(map) && !map.GetShieldCoordinator().HasActiveShield();
+		}
+	}
+}
