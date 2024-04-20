3f5561db9cf7704d5b92ec6d513c131dbaaf6c30
Starforge <mail.starforge@gmail.com>
2024-04-20 05:33:32 +0300

    add support for v1.5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RT_SolarFlareShield
 RimWorld mod; see [About.xml](../master/About/About.xml) and [forum thread](https://ludeon.com/forums/index.php?topic=11272).
+Version 1.5 support AS-IS by Starforge (t.me/Starforge) Sorry, I don't know c-like languages and how to build. But you can check code and build by yourself if needed. 
---
+++ v1.5/Source/Patch_PowerNetTick.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace RT_SolarFlareShield
+{
+	[HarmonyPatch(typeof(PowerNet))]
+	[HarmonyPatch(nameof(PowerNet.PowerNetTick))]
+	internal class Patch_PowerNetTick
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(
+			MethodBase original,
+			IEnumerable<CodeInstruction> instructions)
+		{
+			return instructions.MethodReplacer(
+				AccessTools.Method(typeof(GameConditionManager), nameof(GameConditionManager.ElectricityDisabled)),
+				AccessTools.Method(typeof(Patch_PowerNetTick), nameof(Patch_PowerNetTick.ElectricityDisabled)));
+		}
+
+        public bool ElectricityDisabled(Map map)
+        {
+            foreach (GameCondition activeCondition in map.gameConditionManager.ActiveConditions)
+            {
+                if (activeCondition.ElectricityDisabled && !activeCondition.HiddenByOtherCondition(map) && !map.GetShieldCoordinator().HasActiveShield())
+                {
+                    return true;
+                }
+            }
+            return Find.World.GameConditionManager.Parent?.ElectricityDisabled(map) ?? false;
+        }
+    }
+}
